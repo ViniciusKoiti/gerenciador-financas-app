77cf0dee6f5935508a8142d9bf6cae2fca9823a7
Vinicius Koiti <viniciusnakahara@gmail.com>
2025-02-22 15:48:12 -0300

    Iniciando gráficos da aplicação

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './shared/interceptor/auth.interceptor';
-import {errorInterceptor} from '@shared/interceptor/error.interceptor';
+import { errorInterceptor } from '@shared/interceptor/error.interceptor';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
-    provideHttpClient(
-      withInterceptors([authInterceptor, errorInterceptor])
-    )
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
+    provideCharts(withDefaultRegisterables())
   ]
 };
--- src/app/shared/components/report-graph/report-graph.component.ts
+++ src/app/shared/components/report-graph/report-graph.component.ts
@@ -1,18 +1,45 @@
-import { Component } from '@angular/core';
-import {MatAccordion, MatExpansionPanel} from '@angular/material/expansion';
+import {Component, ViewChild} from '@angular/core';
+import {MatAccordion, MatExpansionPanel, MatExpansionPanelHeader} from '@angular/material/expansion';
 import {MatIcon} from '@angular/material/icon';
+import {BaseChartDirective} from 'ng2-charts';
 
 @Component({
   selector: 'app-report-graph',
   imports: [
     MatExpansionPanel,
     MatAccordion,
-    MatIcon
+    MatExpansionPanelHeader,
+    MatIcon,
+    BaseChartDirective
   ],
   templateUrl: './report-graph.component.html',
   styleUrl: './report-graph.component.scss',
   standalone: true
 })
 export class ReportGraphComponent {
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
+
+  public barChartOptions = {
+    responsive: true,
+    animation: {
+      duration: 1000,
+      easing: 'easeInOutQuad'
+    }
+  };
+  public barChartLabels = ['Janeiro', 'Fevereiro', 'Março', 'Abril'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData = [
+    { data: [50, 70, 40, 90], label: 'Vendas 2024' },
+    { data: [30, 80, 60, 100], label: 'Vendas 2023' }
+  ];
+
+  updateChart() {
+    setTimeout(() => {
+      this.chart?.update();
+    }, 300);
+  }
 }
