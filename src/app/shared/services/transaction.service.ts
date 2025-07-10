9c466fd15e6477adb3c373f9537b12e7e8ae4639
Vinicius Koiti <viniciusnakahara@gmail.com>
2025-07-09 22:35:12 -0300

    Corrigindo endpoint ao atualizar posição de categoria

--- src/app/shared/services/transaction.service.ts
+++ src/app/shared/services/transaction.service.ts
@@ -10,7 +10,7 @@ import {map} from 'rxjs/operators';
   providedIn: 'root'
 })
 export class TransactionService {
-  private readonly baseUrl = `/api/transacoes`;
+  private readonly baseUrl = `/transacoes`;
 
   constructor(private http: HttpClientService) {}
 
